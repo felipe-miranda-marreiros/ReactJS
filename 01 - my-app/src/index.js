25ab7ff91824eda697142bcea893e6c126fe4705
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-03-08 10:44:42 -0300

    Configurações iniciais - npm, create-app

---
+++ 01 - my-app/src/index.js
@@ -0,0 +1,27 @@
+//Import the React and ReactDOM libraries
+import React from "react";
+import ReactDOM from "react-dom";
+
+//Create a React Component
+
+//Function Component
+const App = () => {
+  //Objeto
+  const buttonText = { content: "Click" };
+  
+  //JSX
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText.content}
+      </button>
+    </div>
+  );
+};
+
+//Take the React Component and show it on the screen
+ReactDOM.render(<App />, document.querySelector("#root"));
