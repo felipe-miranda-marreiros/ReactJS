7795af25f9f746268eac35a8dd1b168ebdc7b1a7
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-01 07:41:57 -0300

    Exemplo de form com state

---
+++ 04 - exemplo Form/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Form from "./Forms";
+import "./style.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Form />);
