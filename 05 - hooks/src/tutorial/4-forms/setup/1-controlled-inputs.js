d21493458105a1f627ee00f115e3eb65425e4662
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-05-01 10:21:29 -0300

    Controlled inputs - Forms: name, value, onChange

--- 05 - hooks/src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ 05 - hooks/src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,17 +1,27 @@
 import React, { useState } from "react";
-// JS
-// const input = document.getElementById('myText');
-// const inputValue = input.value
-// React
-// value, onChange
 
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState("");
   const [email, setEmail] = useState("");
+  const [people, setPeople] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+
+    if (firstName && email) {
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName,
+        email,
+      };
+
+      setPeople((people) => {
+        return [...people, person];
+      });
+
+      setFirstName("");
+      setEmail("");
+    }
   };
 
   return (
@@ -42,6 +52,17 @@ const ControlledInputs = () => {
 
           <button type="submit">add person</button>
         </form>
+
+        {people.map((person) => {
+          const { id, firstName, email } = person;
+          return (
+            <div key={id} className="item">
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          );
+        })}
+        
       </article>
     </>
   );
